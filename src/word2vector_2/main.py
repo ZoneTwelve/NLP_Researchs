394946002730b056d5cedfbb326622a12b055364
ZoneTwelve <zonetwelve159@gmail.com>
2023-06-06 12:37:26 +0800

    [update] split in regex

--- src/word2vector_2/main.py
+++ src/word2vector_2/main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import os, time, json
-from re import match
+from re import match, split
 from tqdm import tqdm
 from argparse import ArgumentParser
 # https://dboyliao.medium.com/python-%E8%B6%85%E5%A5%BD%E7%94%A8%E6%A8%99%E6%BA%96%E5%87%BD%E5%BC%8F%E5%BA%AB-argparse-4eab2e9dcc69
@@ -24,9 +24,10 @@ keep = list(filter(lambda v: match(_rp, v), dslist))
 for doc in tqdm(keep):
   doc_path = f'{_ds}/{doc}'
   # https://stackoverflow.com/questions/10971033/backporting-python-3-openencoding-utf-8-to-python-2
-  tokens = open(doc_path, 'r', encoding=_coded, errors='replace').read()
+  tokens = split('\s', open(doc_path, 'r', encoding=_coded, errors='replace').read())
   for token in tokens:
     vocab.add(token)
+
 maps = {index:word for index, word in enumerate(vocab)}
 # https://www.geeksforgeeks.org/get-current-timestamp-using-python/
 output = open(f"_output/{time.time()}.out", "w")
